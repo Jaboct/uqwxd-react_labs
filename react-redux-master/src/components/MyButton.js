f2f86f3e63c9d43e73625b26e8f77a693da47d6c
Jaboct <jaboct@jaboct.com>
2023-12-27 00:11:35 -0500

    finished redux lab

--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,13 @@
+import React from 'react'
+import { useDispatch} from 'react-redux';
+import increment from '../actions'
 
+const MyButton = ()=>{
+    // what is the value of using dispatch here?
+    let dispatch = useDispatch();
+    return (
+        <button onClick={()=>dispatch(increment(1))}>Increase counter</button>
+    );
+}
+
+export default MyButton;
